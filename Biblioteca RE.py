3b796ba922fa5ce697273bacb86f2519ce5636a9
Thiago Araujo <161383143+Thiago-Arauj@users.noreply.github.com>
2024-04-22 18:56:04 -0300

    Update Biblioteca RE.py
    Biblioteca RE funcionando

--- Biblioteca RE.py
+++ Biblioteca RE.py
@@ -2,8 +2,9 @@ import re
 
 def validate_numero_telefone(phone_number):
 
-    # pattern = re.compile(r'([0-9]{2}) 9[0-9]{4}-[0-9]{4}')
-    match = re.fullmatch('\d{2}\s9\d{4}-\d{4}', phone_number)
+    # \ serve para dizer que quero especificamente aquele simbolo
+    pattern = '\(\d{2}\)\s9\d{4}-\d{4}'
+    match = re.fullmatch(pattern, phone_number)
 
     if match:  
       return 'Número de telefone válido.'
@@ -14,8 +15,3 @@ phone_number = input()
 result = validate_numero_telefone(phone_number)
 
 print(result)
-
-# lista = [1, 2, 3, 'a', 'b', 'c']
-
-# for i, v in enumerate(lista):
-#     print(f'Indice={i} Valor={v}')
